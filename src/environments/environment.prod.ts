005794ea160b36eae9f87a779814428556c814bf
(410)Nong'P <65143410@g.cmru.ac.th>
2026-01-26 16:35:48 +0700

    2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import packageInfo from '../../package.json';
 
 export const environment = {
-  appVersion: packageInfo.version,
-  production: true
+  production: true,
+  appVersion: 'v1.0.0',
+  apiUrl: 'https://buddy-pet-shop-service.onrender.com'
 };
