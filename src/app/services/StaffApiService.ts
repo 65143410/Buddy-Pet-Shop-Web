4e5d9b0aa2696a7801ea60b22605fd85ec913382
(410)Nong'P <65143410@g.cmru.ac.th>
2026-02-02 09:36:47 +0700

    feat: create StaffApiService to manage staff and order-related API interactions.

--- src/app/services/StaffApiService.ts
+++ src/app/services/StaffApiService.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Order, Staff } from '../demo/models/product.model';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
